632fb59836dfb677b7f65b28f416d139b8d7aa28
Herbert Ilhan Tanujaya <mail@herbert.id>
2019-10-11 10:50:25 +0800

    Typo on_update/onupdate (#30)

--- src/database.py
+++ src/database.py
@@ -26,7 +26,7 @@ class Base(_base):
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(
-        DateTime(timezone=True), server_default=func.now(), on_update=func.now()
+        DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
     )
 
     def asdict(self):
